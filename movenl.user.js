12f9e5d82ad73f237e0be0e406bec7cab1fe5c40
bmi-nl <43075336+bmi-nl@users.noreply.github.com>
2021-12-07 18:56:02 +0100

    Update movenl.user.js

--- movenl.user.js
+++ movenl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Move.nl
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://move.nl/verkoop-dossier/*/agenda
